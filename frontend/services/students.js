37563d42607a12dd4dd9e9684c67dfabca4110a2
Faris <allouchfaris21@gmail.com>
2025-06-09 01:15:57 +0200

    Deployment test

--- frontend/services/students.js
+++ frontend/services/students.js
@@ -2,7 +2,7 @@ var StudentService = {
     reload_students_datatable : function () {
         Utils.get_datatable(
             "tbl_students",
-            Constants.API_BASE_URL + "students",
+            Constants.get_api_base_url() + "students",
             [
                 { data: "action"},
                 { data: "FirstName"},
--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,3 +1,9 @@
 var Constants = {
-    API_BASE_URL: "http://localhost/FarisAllouch/Student-Management-System/backend/"
+    get_api_base_url: function () {
+        if(location.hostname == 'localhost') {
+            return "http://localhost/FarisAllouch/Student-Management-System/backend/"
+        } else {
+            return "https://squid-app-8obkj.ondigitalocean.app/backend/"
+        }
+    }
 }
